e952cfece466d54ca1368936058448399baca2ed
Angelina Li <ali6@wellesley.edu>
2018-03-26 18:26:10 -0400

    updated stuff

--- config.py
+++ config.py
@@ -2,9 +2,3 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY", "some-super-duper-secret-key")
-    UPLOAD_FOLDER = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), 
-        "app",
-        "tmp", 
-        "ical_files")
-    MAX_CONTENT_PATH = 100000
