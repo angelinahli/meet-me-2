befb939f289c12749d88d1d3bf2e7e627c1d54ec
Angelina Li <ali6@wellesley.edu>
2018-03-26 20:15:10 -0400

    made shell context

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-from app import app
+from app import app, db
+from app.models import User, Event
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"db": db, "User": User, "Event": Event}
 
 if __name__ == "__main__":
     app.run(debug=True)
