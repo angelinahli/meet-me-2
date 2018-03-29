3aa7cacf66a5b8fcebeabccd6542fcd0f6be2808
Angelina Li <ali6@wellesley.edu>
2018-03-29 15:47:14 -0400

    update venv

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,6 @@ from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
 from app.forms import LoginForm, SignUpForm, SettingsForm
 from app.models import User, Event
-from program.exceptions import FlashException
 
 @app.route("/")
 @app.route("/index/")
