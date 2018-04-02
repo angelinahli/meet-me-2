54366a35db2204ec7f1b0f4f954085d9dd388ff7
Angelina Li <ali6@wellesley.edu>
2018-04-01 23:51:25 -0400

    updated test db

--- tests/generate_events.py
+++ tests/generate_events.py
@@ -5,6 +5,7 @@ date: 03/07/2018
 desc: helper script to generate some events for test database
 """
 
+import csv
 import random
 from datetime import datetime, timedelta
 from pytz import utc # timezone
@@ -15,7 +16,7 @@ def get_string(n):
 def generate_fake_dates(n):
     dates = []
     for i in xrange(n):
-        year = random.choice(range(2017,2019))
+        year = 2018
         month = random.choice(range(1,13))
         day = random.choice(range(1,29))
         hour = random.choice(range(0,24))
@@ -27,7 +28,6 @@ def generate_fake_dates(n):
             hour=get_string(hour),
             min=get_string(minutes)
         )
-        print datetime
         dates.append(datetime)
     return dates
 
@@ -47,20 +47,25 @@ def get_fake_usernames(n):
         "chiwetel",
         "lunadiego"
     ]
+    new_users = []
     for i in xrange(n):
-        print random.choice(users)
+        new_users.append(random.choice(users))
+    return new_users
 
 def get_datetime(date_text):
     return utc.localize(datetime.strptime(date_text, "%Y-%m-%d-%H-%M"))
 
 def get_second_dates(first_dates):
+    second_dates = []
     for date_string in first_dates:
         date = get_datetime(date_string)
         add = timedelta(minutes=random.choice(range(15,60*25,15)))
         new_date = date + add
-        print new_date.strftime("%Y-%m-%d-%H-%M")
+        second_dates.append(new_date.strftime("%Y-%m-%d-%H-%M"))
+    return second_dates
 
 def get_descriptions(n):
+    descs = []
     desc = [
         "meal",
         "interview",
@@ -85,16 +90,29 @@ def get_descriptions(n):
         "kayaking",
         "baton twirling",
         "knitting",
-        "dance"
+        "dance",
+        "beach",
+        "pub night",
+        "trivia",
+        "cleaning"
     ]
     for i in xrange(n):
-        print random.choice(desc)
+        descs.append(random.choice(desc))
+    return descs
 
-print "\nFIRST DATES\n"
-dates = generate_fake_dates(200)
-print "\nSECOND DATES\n"
-get_second_dates(dates)
-print "\nUSERS\n"
-get_fake_usernames(200)
-print "\nDESCS\n"
-get_descriptions(200)
+def make_csv(filename, n):
+    start_dates = generate_fake_dates(n)
+    end_dates = get_second_dates(start_dates)
+    usernames = get_fake_usernames(n)
+    descs = get_descriptions(n)
+    with open(filename, "w") as fl:
+        headings = ["username", "desc", "start_dt", "end_dt"]
+        writer = csv.writer(fl)
+        writer.writerow(headings)
+        for i in range(n):
+            data = [usernames[i], descs[i], start_dates[i], end_dates[i]]
+            writer.writerow(data)
+    print("Done!")
+
+if __name__ == "__main__":
+    make_csv("files/test_events.csv", 500)
